9da12129e07f1667fc2a20866daa076d09b080f9
quangkeu <nlequang1@gmail.com>
2017-08-01 08:46:15 +0700

    Create test-jenkinsfile.groovy

---
+++ test-jenkinsfile.groovy
@@ -0,0 +1,18 @@
+podTemplate(label: 'maven', containers: [
+    containerTemplate(
+        name: 'maven', 
+        image: 'maven:3-jdk-8-alpine', 
+        ttyEnabled: true, 
+        command: 'cat')
+    ]) {
+        node('maven') {
+            stage('Build a Maven project') {
+                git 'https://github.com/quangkeu/jenkins-k8s.git'
+                container('maven') {
+                    sh """
+                    mvn clean deploy
+                    """
+                }
+            }
+        }
+    }
