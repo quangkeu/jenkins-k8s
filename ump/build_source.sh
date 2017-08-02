4ba670ea0d02ccdd7a514dc866b7aa3f011130aa
quangkeu <nlequang1@gmail.com>
2017-08-02 08:36:21 +0700

    Update build_source.sh

--- ump/build_source.sh
+++ ump/build_source.sh
@@ -7,4 +7,5 @@ cd $homeDir/ump/ump-core/ && mvn clean deploy
 # Build ump-backend
 cd $homeDir/ump/ump-backend && mvn clean deploy
 
-# B
+# Build ump-webapp
+cd $homeDir/ump/ump-webapp && mvn clean deploy
